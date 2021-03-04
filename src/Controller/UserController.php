ae74ea10c136f9e9a52ebd7e46a6f35c10c13dc6
frehder <hi@florianrehder.de>
2021-03-04 18:34:21 +0100

    add create user endpoint
    POST /api/v1/user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,15 +3,32 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\Type\UserType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class UserController extends AbstractController
+class UserController extends AbstractApiController
 {
     public function list(): Response
     {
         $user = $this->getDoctrine()->getRepository(User::class)->findAll();
 
         return $this->json($user);
+    }
+
+    public function create(Request $request): Response
+    {
+        $form = $this->buildForm(UserType::class);
+        $form->handleRequest($request);
+
+        if (!$form->isSubmitted() || !$form->isValid()) {
+            return $this->respond($form, Response::HTTP_BAD_REQUEST);
+        }
+
+        $user = $form->getData();
+        $this->getDoctrine()->getManager()->persist($user);
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->json(Response::HTTP_CREATED);
     }
 }
